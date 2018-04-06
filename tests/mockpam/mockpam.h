a19fdae399fbdb144abaebf4a3e4cb9ccc97c388
David Llewellyn-Jones <david@flypig.co.uk>
2018-04-06 20:12:19 +0100

    Detect whether header is _pam_types.h or pam_types.h
    For some reason macOS doesn't have the underscore prefix on
    security/_pam_types.h

--- tests/mockpam/mockpam.h
+++ tests/mockpam/mockpam.h
@@ -1,5 +1,16 @@
-//#include <security/_pam_types.h>
-#include <security/pam_types.h>
+#include "config.h"
+
+#ifdef HAVE_SECURITY__PAM_TYPES_H
+	// Used on Linux, and other systems apparently
+	#include <security/_pam_types.h>
+#else //  // #ifdef HAVE_SECURITY__PAM_TYPES_H
+	#ifdef HAVE_SECURITY_PAM_TYPES_H
+		// Used on macOS for some reason
+		#include <security/pam_types.h>
+	#else // #ifdef HAVE_SECURITY_PAM_TYPES_H
+		#error "_pam_types.h header file is needed"
+	#endif // #ifdef HAVE_SECURITY_PAM_TYPES_H
+#endif // #ifdef HAVE_SECURITY__PAM_TYPES_H
 
 typedef struct {
 	int (*pam_set_item)(pam_handle_t *pamh, int item_type, const void *item);
